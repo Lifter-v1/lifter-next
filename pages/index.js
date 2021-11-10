d4af561ce8714ba0307bc426e98f25519c2310d3
michaelsatumba <mjsatumba@gmail.com>
2021-11-10 12:48:12 -0800

    first commit

---
+++ pages/index.js
@@ -0,0 +1,16 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import tw from "tailwind-styled-components"
+
+
+export default function Home() {
+  return (
+    <Wrapper>
+      Hello
+    </Wrapper>
+  )
+}
+
+const Wrapper = tw.div`
+
+`
